bb99ecc673f2feada9573232480a24aabe28b686
jayezhou <1282267168@qq.com>
2024-03-15 19:26:04 +0800

    Update h8900.py

--- selenium/h8900.py
+++ selenium/h8900.py
@@ -1,14 +1,17 @@
 # pip install selenium
+# pip install openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from openpyxl import load_workbook
 
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-proxy-certificate-handler")
 options.add_argument("--allow-running-insecure-content")
 options.add_argument("--ignore-certificate-errors")
 
+# 打开要处理的页面
 # driver = webdriver.Chrome()
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
@@ -27,20 +30,30 @@ driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').send_keys("东
 driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]/../div/button[1]').click()
 driver.find_element(By.XPATH, '//button[contains(text(), "人员信息")]').click()
 
-driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').clear()
-time.sleep(2)
-driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').send_keys("张飞")
-driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]/../div/button[1]').click()
-time.sleep(2)
-driver.find_element(By.XPATH, '//*[@title="删除"]').click()
-time.sleep(2)
-driver.switch_to.default_content()
-driver.find_element(By.XPATH, '//*[@value="确定删除"]').click()
-driver.switch_to.frame('iframe-FACE')
-driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').clear()
-time.sleep(2)
-driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').send_keys("张飞")
-driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]/../div/button[1]').click()
+# 加载工作簿
+wb = load_workbook('work.xlsx')
+# 获取sheet页
+ws = wb['person_chengxiang']
+for i in range(2, 2644):  # 2是Excel表格的开始行，2644是结束行
+    name = ws.cell(row=i, column=9).value  # 9是要处理的人员名字所在列
+    driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').clear()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').send_keys(name)
+    driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]/../div/button[1]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@title="删除"]').click()
+    time.sleep(1)
+    driver.switch_to.default_content()
+    driver.find_element(By.XPATH, '//*[@value="确定删除"]').click()
+    driver.switch_to.frame('iframe-FACE')
+    # driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').clear()
+    # time.sleep(1)
+    # driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]').send_keys(name)
+    # driver.find_element(By.XPATH, '//*[@placeholder="模糊查询"]/../div/button[1]').click()
+    # time.sleep(1)
+    print('已处理：' + name)
+    ws.cell(row=i, column=12).value ='1'
+    wb.save('work.xlsx')
+wb.close()
 
-time.sleep(5)
 driver.close()
