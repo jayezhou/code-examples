3bf487aae384d866272d020a3ae27718b7852f56
jayezhou <1282267168@qq.com>
2024-10-17 15:14:10 +0800

    删掉一些多余的代码

--- arco-admin/src/views/other/page-header-nav/components/comp-a.vue
+++ arco-admin/src/views/other/page-header-nav/components/comp-a.vue
@@ -3,22 +3,11 @@
   <span>count: {{ count }}</span>
   <button @click="count++">+</button>
 
-  <a-link @click="handleClick">Link</a-link>
 </template>
 
 <script setup>
   import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-
-
-  const router = useRouter();
 
   const count = ref(0);
 
-  const handleClick = (value) => {
-    // Trigger router change
-    router.push({
-      name: 'SearchTable',
-    });
-  };
 </script>
