22fabd03cf233a63422315b8bf2d819c931611a3
jayezhou <1282267168@qq.com>
2025-04-03 14:10:00 +0800

    Deleted pyexe's setup.py

--- chatbot_llm/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup  
-import py2exe  
- 
-setup(  
-    console=['chatbot_deepseek.py'],  # 如果是控制台应用  
-    # windows=['your_script.py'],  # 如果是 GUI 应用  
-)
-
-# python setup.py py2exe
